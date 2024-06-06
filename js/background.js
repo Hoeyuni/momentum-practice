41eadcfbaa3ca6d8baa0b903b3a641c66355f2a9
Hoeyun <yang3522@gmail.com>
2024-06-06 18:27:28 +0900

    Updated background.js - delete console.log

--- js/background.js
+++ js/background.js
@@ -2,8 +2,6 @@ const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "
 
 const choseImage = images[Math.floor(Math.random() * images.length)];
 
-console.log("choseImage : ", choseImage);
-
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${choseImage}`;
